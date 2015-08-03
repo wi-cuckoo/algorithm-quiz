e4b1f88170bb98e557a269a8ae08809772baa254
wi-cuckoo <yp_liuwei@163.com>
2015-08-03 21:33:14 +0800

    2015/8/3

---
+++ adph.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int maxday (int ,int, int day[]);
+void output (int, int rel[]);
+
+int main (void)
+{
+	int num, N, M;
+	
+	scanf ("%d", &num);
+	
+	int out[num];
+
+	int k = 0;
+	while (k < num)
+	{
+		scanf ("%d %d", &N, &M);
+
+		int day[N];
+		int i = 0;
+		while ( i < N)
+			scanf ("%d", &day[i++]);
+
+		if (N < M)
+		{
+			out[k++] = 100;	
+			continue;
+		}
+		else out[k++] = maxday (N, M, day);
+	}	
+
+	output (num, out);
+
+	return 0;
+}
+
+int maxday (int n, int m, int day[])
+{
+	int max = day[m] - 1 ;
+	int j = 0;
+	while ((j+m+1) < n)
+	{
+		int temp = day[j+m+1] - day[j++] - 1;
+		max = temp > max? temp: max;
+	}
+
+	return max;
+}
+
+void output (int num, int rel[])
+{
+	int i = 0;
+	while (i < num)
+		printf("%d\n", rel[i++]);
+}
---
+++ mt.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+
+int check (int i, char s[])
+{
+	char test1[] = "rshtomp";
+	char test2[] = "RSHTOMP";
+
+	for (int j=0; j < 7; j++)
+	{
+		if (test1[j] == s[i+j] || test2[j] == s[i+j])
+			continue;
+		else 
+			return 1;
+	}
+
+	return 0;
+}
+
+void replace (char str[])
+{
+	char repl[] = "fjxmlhx00";
+	int i = 0;
+
+	while (str[i] != '\0' )
+	{
+		if (str[i] == 'M' || str[i] == 'm' )
+		{
+			if (str[i+1] == 'A' || str[i+1] == 'a')
+			{
+				if (!check(i+2, str))
+				{
+					int k = 0;
+					while (k < 9)
+						str[i++] = repl[k++];
+				}
+				else
+				{
+					i++; continue;
+				}
+			}
+			else
+			{
+				i++; continue;
+			}
+		}
+		else 
+		{
+			i++; continue;
+		}
+	}
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '0') 
+		{
+			i++;
+			continue;
+		}
+		printf ("%c", str[i++]);
+	}
+
+}
+
+int main (void)
+{
+	char instr[1024][200];
+
+	int count = 0;
+	while (fgets (instr[count], 200, stdin)) count++;
+
+	int i = 0;
+	while (i < count)
+	{
+		char str[100];
+		strcpy (str, instr[i++]);
+//		printf ("%s", str);
+		replace (str);	
+	}
+
+	return 0;
+}
